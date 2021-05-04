dad851b62bd9de7b051b1a5550b2e7e9fb2440e6
Gaurav Vaidya <gaurav@ggvaidya.com>
2021-05-04 17:00:21 -0400

    Removed non-functional tests.

--- tests/conftest.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from pathlib import Path
-
-
-@pytest.fixture(scope='session')
-def yaml_files():
-    files = list(Path(__file__).parent.parent.joinpath('model').glob(r'*.yaml'))
-    return {p.name: str(p) for p in files if p.is_file()}
