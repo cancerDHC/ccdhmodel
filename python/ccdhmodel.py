1c344231efdf888c000b1995becf16e07e75db68
Gaurav Vaidya <gaurav@ggvaidya.com>
2021-05-31 14:54:36 -0400

    Regenerated artifacts.

--- python/ccdhmodel.py
+++ python/ccdhmodel.py
@@ -1,5 +1,5 @@
 # Auto generated from ccdhmodel.yaml by pythongen.py version: 0.9.0
-# Generation date: 2021-05-31 13:55
+# Generation date: 2021-05-31 14:47
 # Schema: CRDC-H
 #
 # id: https://example.org/ccdh
