17eb1de35e3147239583ecb4dd7e46ef7c3562f4
Mark Andrew Miller <MAM@lbl.gov>
2021-08-11 09:18:53 -0400

    crdch rather than ccdhmodel in setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 from warnings import warn
 
 if sys.version_info < (3, 7, 0):
-    warn(f"Python version 3.7 or later is required for ccdhmodel.  Current version: {sys.version_info}")
+    warn(f"Python version 3.7 or later is required for the crdch model.  Current version: {sys.version_info}")
     sys.exit(1)
 
 setup(
