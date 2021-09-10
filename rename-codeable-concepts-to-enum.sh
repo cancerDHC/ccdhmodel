c976f7e4c789c9415e262a62489e83ca59ceaf4a
Gaurav Vaidya <gaurav@ggvaidya.com>
2021-09-10 13:48:03 -0400

    Fixed macOS-specific issue, regenerated model.

--- rename-codeable-concepts-to-enum.sh
+++ rename-codeable-concepts-to-enum.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-sed -i 's/value_codeable_concept/value_enum/g' model/schema/crdch_model.yaml > temp.yaml
+sed -i '' 's/value_codeable_concept/value_enum/g' model/schema/crdch_model.yaml > temp.yaml
